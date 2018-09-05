5d78b0ed6f422477a2dd7c57eb676d5eb3b5b2da
Vlad Rez <vladrezdev@gmail.com>
2018-09-04 19:59:01 -0700

    Added JavaScript README and basic syntax example

---
+++ javascript/syntax-basics.variables.js
@@ -0,0 +1,17 @@
+//var keyword + identifier + ;
+var color;
+/* 
+By itself the Js console will return an "undifined" 
+since the actual statement doesn't explicitly do anything 
+however behind the scenes we're telling the Js engine that
+we're are going to want to assign a value in a variable called 'color'
+*/
+/* 
+There are some restrictions on variable names, for example
+names cannot start with numbers 
+*/
+/* 
+The equal sign like most languages is the assingment operator.
+*/
+color = "Red";
+
