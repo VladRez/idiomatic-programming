32d1afa5a1df3d405478f2c15209f5bb9bb5dcd4
Vlad Rez <vladrezdev@gmail.com>
2018-09-06 17:06:57 -0700

    Removed syntax-basics examples, plan to consolidate basics

---
+++ JavaScript/language-basics.js
@@ -0,0 +1,15 @@
+
+//This is a comment
+/**
+ * This is a C-Style
+ *  multiline comment
+ */
+
+//Everything is case sensitive 'name' is different from 'Name'
+
+/*
+* Identifier: variable, function, property, or function argument name
+* fist character must be a letter, underscore _ , or a dollar sign $
+*   
+*
+*/
